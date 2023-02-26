56b11cf676a3a0923489b28730880fe7baec5bf1
Aadarshab <72687881+Aadarshab@users.noreply.github.com>
2023-02-26 14:08:37 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -6,7 +6,7 @@ DROPLINK_API = '1234'
 MDISK_API = 'abcd35'
 ADMINS = [1861030649, 1798040070]
 DATABASE_NAME = 'MdiskConvertor'
-DATABASE_URL = 'mongodb+srv://username:pwd@cluster0.xxxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+DATABASE_URL = 'mongodb+srv://Movie:AB@cluster0.g1lthiw.mongodb.net/?retryWrites=true&w=majority'
 
 
 
@@ -16,9 +16,9 @@ EXCLUDE_DOMAIN = ['t.me', 'telegram.me']
 CHANNELS = True
 CHANNEL_ID = [-1001798040070, -1001194853986]
 FORWARD_MESSAGE = True
-SOURCE_CODE = 'https://github.com/CyniteOfficial/Channel-Mdisk-Converter'
-USERNAME = 'Cynitebackup'
-HEADER_TEXT = '**Join My Channel** [CyniteBackup](https://t.me/CyniteBackup)\n\n'
+SOURCE_CODE = 'https://github.com/Aadarshab/Channel-Mdisk-Converter'
+USERNAME = 'abcracked_movie'
+HEADER_TEXT = '**Join My Channel** [Abcracked_movie](https://t.me/Abcracked_movie)\n\n'
 FOOTER_TEXT = '\n\n Share and Support'
 BANNER_IMAGE = 'https://cdn.pixabay.com/photo/2019/04/24/21/55/cinema-4153289__480.jpg'
 WELCOME_IMAGE = 'https://media.istockphoto.com/photos/hello-and-welcome-written-white-lightbox-sitting-on-blue-background-picture-id1371547852?b=1&k=20&m=1371547852&s=170667a&w=0&h=vRGvMO4a7KKYdTQ9Ln1UVdlg5C0POExw73jGsotVgzA='
